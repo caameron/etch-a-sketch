6f92d1bf573c5eee7bf719729e5c1026ddae7f1c
Caameron <caamnaka@gmail.com>
2022-10-29 13:48:13 -0700

    In process of adding grid 16 by 16 grid

---
+++ etch.js
@@ -0,0 +1,11 @@
+function createSquareGrid(dimension) {
+    //grab grid-container
+    let gridContainer = document.getElementById('grid-container');
+
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('grid-block');
+    console.log(gridContainer);
+}
+
+
+createSquareGrid(16);
