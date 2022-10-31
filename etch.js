e88c5686ea82617e437e9f25a8dea20a416765bb
caameron <caamnaka@gmail.com>
2022-10-30 20:15:45 -0700

    on hover event added to change background color to red

--- etch.js
+++ etch.js
@@ -2,14 +2,22 @@ function createSquareGrid(dimension) {
     //grab grid-container
     let gridContainer = document.getElementById('grid-container');
 
-
+    let widthAndHeight = 500 / dimension;
+    console.log(widthAndHeight);
     for(let count = 0; count < dimension*dimension; count ++){
         let newDiv = document.createElement('div');
         newDiv.classList.add('grid-block');
+        newDiv.style.width = widthAndHeight + 'px';
+        newDiv.style.height = widthAndHeight + 'px';
+        newDiv.addEventListener('mouseover', (event) => {changeBackgroundColor(event)});
         gridContainer.appendChild(newDiv);
     }
 
+}
 
+function changeBackgroundColor(event){
+    // console.log(this);
+    event.target.style.backgroundColor = 'red';
 }
 
 
