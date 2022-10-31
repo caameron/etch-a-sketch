dcbdef4c8f9f598c1993b7a69da2e67dc6c3150f
caameron <caamnaka@gmail.com>
2022-10-30 19:51:07 -0700

    grid is up

--- etch.js
+++ etch.js
@@ -2,9 +2,14 @@ function createSquareGrid(dimension) {
     //grab grid-container
     let gridContainer = document.getElementById('grid-container');
 
-    let newDiv = document.createElement('div');
-    newDiv.classList.add('grid-block');
-    console.log(gridContainer);
+
+    for(let count = 0; count < dimension*dimension; count ++){
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('grid-block');
+        gridContainer.appendChild(newDiv);
+    }
+
+
 }
 
 
